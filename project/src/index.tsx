b90643b8fa9ebd9035165a5c9107a4cf381349f5
Shurafads <wypafads@gmail.com>
2023-03-11 17:21:57 +0300

    Создает страницу 404 и описывает маршруты для компонентов

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const Setting = {
+  CountPlaces: 320
+} as const;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App countPlaces = {Setting.CountPlaces} />
+    </HelmetProvider>
   </React.StrictMode>,
 );
