f561e879c89119383bc2fcd58c37380c0c637ba4
Shurafads <wypafads@gmail.com>
2023-03-27 16:48:39 +0300

    Добавлет переключение карты при выборе города

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -11,6 +11,7 @@ type MapProps = {
 
 function Map({className, height}: MapProps): JSX.Element {
 
+
   const OffersList = useAppSelector((state) => state.offersList);
 
   const currentCityData = OffersList[0].city;
@@ -24,20 +25,35 @@ function Map({className, height}: MapProps): JSX.Element {
     iconAnchor: [14, 0],
   });
 
+  const currentCustomIcon = leaflet.icon({
+    iconUrl: 'img/pin-active.svg',
+    iconSize: [28, 40],
+    iconAnchor: [14, 0],
+  });
+
   useEffect(() => {
+
     if (map) {
+      map.flyTo([
+        currentCityData.location.latitude,
+        currentCityData.location.longitude
+      ], currentCityData.location.zoom);
+
       OffersList.forEach((offer) => {
         leaflet
           .marker({
             lat: offer.location.latitude,
             lng: offer.location.longitude,
           }, {
-            icon: defaultCustomIcon,
+            icon: (offer.id)
+              ? defaultCustomIcon
+              : currentCustomIcon,
           })
           .addTo(map);
       });
     }
-  }, [map, OffersList, defaultCustomIcon]);
+
+  }, [map, OffersList, defaultCustomIcon, currentCityData, currentCustomIcon]);
 
   return (
     <section className={className} style={{height: height}} ref={mapRef}></section>
