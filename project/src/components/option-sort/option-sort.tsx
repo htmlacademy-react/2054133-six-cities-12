60b0d9b17df662275107980068040ecf3cc1c7d8
Shurafads <wypafads@gmail.com>
2023-03-29 23:05:10 +0300

    Редактирует название типа пропса

--- project/src/components/option-sort/option-sort.tsx
+++ project/src/components/option-sort/option-sort.tsx
@@ -1,10 +1,10 @@
-type OptionProps = {
+type OptionSortProps = {
   handleClickOption: (arg0: React.MouseEvent<HTMLLIElement>) => void;
   optionClassName: string;
   option: string;
 }
 
-function OptionSort({handleClickOption, optionClassName, option}: OptionProps) {
+function OptionSort({handleClickOption, optionClassName, option}: OptionSortProps) {
   return(
     <li className={`places__option ${option === optionClassName ? 'places__option--active' : ''}`} tabIndex={0} onClick={(evt) => handleClickOption(evt)}>{option}</li>
   );
